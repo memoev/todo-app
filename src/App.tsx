72a25fb1c4e9e599ada8023f27ebb133f32ee1dd
Guillermo Villalta <mexcelus@gmail.com>
2019-10-10 15:28:28 -0600

    removed div with hello world and added components of app rendering Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { TodoListItem } from './TodoListItem'
 
+type Todo = {
+  text: string;
+  complete: boolean;
+}
+
+const todos: Array<Todo> = [
+  {text: "Learn Typescript", complete: false}, 
+  {text: "Walk the dog!", complete: false}
+]
 
 const App: React.FC = () => {
   return (
-
-    <div>Hello World!</div>
-
+    <TodoListItem />
   );
 }
 
