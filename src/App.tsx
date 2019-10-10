be105a9b8322215abc8310e6e8e7ce3ffe0f9b38
Guillermo Villalta <mexcelus@gmail.com>
2019-10-10 15:41:40 -0600

    showing first element of array of todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { TodoListItem } from './TodoListItem'
 
 const todos: Array<Todo> = [
-  {text: "Learn Typescript", complete: false}, 
+  {text: "Learn Typescript", complete: true}, 
   {text: "Walk the dog!", complete: false}
 ]
 
 const App: React.FC = () => {
   return (
-    <TodoListItem />
+    <TodoListItem todo={todos[0]}/>
   );
 }
 
