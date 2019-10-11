71390b1218b72cfe76135ac86d26de2846e7482e
Guillermo Villalta <mexcelus@gmail.com>
2019-10-10 20:52:15 -0600

    typo

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,8 @@ const App: React.FC = () => {
 
   return (
     <React.Fragment>
-      <TodoListItem todo={todos[0]} toogleTodo={toggleTodo}/>
-      <TodoListItem todo={todos[1]} toogleTodo={toggleTodo}/>
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo}/>
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo}/>
     </React.Fragment>
   );
 }
