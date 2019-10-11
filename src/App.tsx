b9ac33b11bc0c57764e2c03b8a6a69f1082623c3
Guillermo Villalta <mexcelus@gmail.com>
2019-10-10 19:43:01 -0600

    added toogle todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoListItem } from './TodoListItem'
 
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   {text: "Learn Typescript", complete: true}, 
   {text: "Walk the dog!", complete: false}
 ]
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <React.Fragment>
-      <TodoListItem todo={todos[0]}/>
-      <TodoListItem todo={todos[1]} />
+      <TodoListItem todo={todos[0]} toogleTodo={toggleTodo}/>
+      <TodoListItem todo={todos[1]} toogleTodo={toggleTodo}/>
     </React.Fragment>
   );
 }
