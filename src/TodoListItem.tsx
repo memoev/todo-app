d6192f927176deb42f71210232e437c0853eb238
Guillermo Villalta <mexcelus@gmail.com>
2019-10-10 20:51:35 -0600

    able toggle items in list

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -3,13 +3,14 @@ import './TodoListItem.css'
 
 interface TodoListItemProps {
     todo: Todo;
+    toggleTodo: ToggleTodo;
 };
 
-export const TodoListItem: React.FunctionComponent<TodoListItemProps> = ({ todo }) => {
+export const TodoListItem: React.FunctionComponent<TodoListItemProps> = ({ todo, toggleTodo }) => {
     return <li>
         {/* Here we add style to the element so it can be crossed if complete is true */}
         <label className={todo.complete ? "complete" : undefined}> 
-            <input type="checkbox" checked={todo.complete}/>
+            <input type="checkbox" checked={todo.complete} onChange={() => toggleTodo(todo)} />
             {todo.text}
         </label>
     </li>;
